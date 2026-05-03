a099cfbbaf8a0c9f04b40471658a92cec5e10ea9
xjrstjrwvg-commits <xjrstjrwvg@privaterelay.appleid.com>
2026-05-03 14:41:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,135 @@
-import os
+import os, time, sys, re
 from flask import Flask, render_template, request, jsonify
+from collections import Counter
 
+sys.setrecursionlimit(10000)
 app = Flask(__name__)
 
-# 配布用：国名データのみ
-COUNTRIES = ["アイスランド", "アイルランド", "アゼルバイジャン", "アフガニスタン", "アメリカ", "アラブシュチョウコクレンポウ", "アルジェリア", "アルゼンチン", "アルバニア", "アルメニア", "アンゴラ", "アンティグアバーブーダ", "アンドラ", "イエメン", "イギリス", "イスラエル", "イタリア", "イラク", "イラン", "インド", "インドネシア", "ウガンダ", "ウクライナ", "ウズベキスタン", "ウルグアイ", "エクアドル", "エジプト", "エストニア", "エスワティニ", "エチオピア", "エリトリア", "エルサルバドル", "オーストラリア", "オーストリア", "オマーン", "オランダ", "ガーナ", "カーボベルデ", "ガイアナ", "カザフスタン", "カタール", "カナダ", "ガボン", "カメルーン", "ガンビア", "カンボジア", "キタマケドニア", "ギニア", "ギニアビサウ", "キプロス", "キューバ", "ギリシャ", "キリバス", "キルギス", "グアテマラ", "クウェート", "クックショトウ", "グレナダ", "クロアチア", "ケニア", "コートジボワール", "コスタリカ", "コソボ", "コモロ", "コロンビア", "コンゴキョウワコク", "コンゴミンシュキョウワコク", "サウジアラビア", "サモア", "サントメプリンシペ", "ザンビア", "サンマリノ", "シエラレオネ", "ジブチ", "ジャマイカ", "ジョージア", "シリア", "シンガポール", "ジンバブエ", "スイス", "スウェーデン", "スーダン", "スペイン", "スリナム", "スリランカ", "スロバキア", "スロベニア", "セーシェル", "セキドウギニア", "セネガル", "セルビア", "セントクリストファーネービス", "セントビンセントグレナディーンショトウ", "セントルシア", "ソマリア", "ソロモンショトウ", "タイ", "ダイカンミンコク", "タジキスタン", "タンザニア", "チェコ", "チャド", "チュウオウアフリカ", "チュウカジンミンキョウワコク", "チュニジア", "チョウセンミンシュシュギジンミンキョウワコク", "チリ", "ツバル", "デンマーク", "ドイツ", "トーゴ", "ドミニカキョウワコク", "ドミニカコク", "トリニダードトバゴ", "トルクメニスタン", "トルコ", "トンガ", "ナイジェリア", "ナウル", "ナミビア", "ニウエ", "ニカラグア", "ニジェール", "ニホン", "ニュージーランド", "ネパール", "ノルウェー", "バーレーン", "ハイチ", "パキスタン", "バチカンシコク", "パナマ", "バヌアツ", "バハマ", "パプアニューギニア", "パラオ", "パラグアイ", "バルバドス", "ハンガリー", "バングラデシュ", "ヒガシティモール", "フィジー", "フィリピン", "フィンランド", "ブータン", "ブラジル", "フランス", "ブルガリア", "ブルキナファソ", "ブルネイ", "ブルンジ", "ベトナム", "ベナン", "ベネズエラ", "ベラルーシ", "ベリーズ", "ペルー", "ベルギー", "ポーランド", "ボスニアヘルツェゴビナ", "ボツワナ", "ボリビア", "ポルトガル", "ホンジュラス", "マーシャルショトウ", "マダガスカル", "マラウイ", "マリ", "マルタ", "マレーシア", "ミクロネシアレンポウ", "ミナミアフリカキョウワコク", "ミナミスーダン", "ミャンマー", "メキシコ", "モーリシャス", "モーリタニア", "モザンビーク", "モナコ", "モルディブ", "モルドバ", "モロッコ", "モンゴル", "モンテネグロ", "ヨルダン", "ラオス", "ラトビア", "リトアニア", "リビア", "リヒテンシュタイン", "リベリア", "ルーマニア", "ルクセンブルク", "ルワンダ", "レソト", "レバノン", "ロシア"]
-
+# パズル用順番表（濁音・半濁音独立、ン→ア環状）
+KANA_LIST = (
+    "アイウエオ" "カキクケコ" "ガギグゲゴ" "サシスセソ" "ザジズゼゾ"
+    "タチツテト" "ダヂヅデド" "ナニヌネノ" "ハヒフヘホ" "バビブベボ"
+    "パピプペポ" "マミムメモ" "ヤユヨ" "ラリルレロ" "ワン"
+)
 SMALL_TO_LARGE = {"ァ": "ア", "ィ": "イ", "ゥ": "ウ", "ェ": "エ", "ォ": "オ", "ッ": "ツ", "ャ": "ヤ", "ュ": "ユ", "ョ": "ヨ", "ヮ": "ワ"}
-VOICE_MAP = str.maketrans("ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ", "カキクケコサシスセソタチツテトハヒフヘホハヒフヘホ")
 
-def get_clean_char(w, pos="head"):
-    char = w if pos == "head" else w.replace("ー", "")[-1]
+def to_katakana(text):
+    if not text: return ""
+    return "".join([chr(ord(c) + 96) if 0x3041 <= ord(c) <= 0x3096 else c for c in text])
+
+DICTIONARY_MASTER = {
+    "country": ["アイスランド", "アイルランド", "アゼルバイジャン", "アフガニスタン", "アメリカ", "アラブシュチョウコクレンポウ", "アルジェリア", "アルゼンチン", "アルバニア", "アルメニア", "アンゴラ", "アンティグアバーブーダ", "アンドラ", "イエメン", "イギリス", "イスラエル", "イタリア", "イラク", "イラン", "インド", "インドネシア", "ウガンダ", "ウクライナ", "ウズベキスタン", "ウルグアイ", "エクアドル", "エジプト", "エストニア", "エスワティニ", "エチオピア", "エリトリア", "エルサルバドル", "オーストラリア", "オーストリア", "オマーン", "オランダ", "ガーナ", "カーボベルデ", "ガイアナ", "カザフスタン", "カタール", "カナダ", "ガボン", "カメルーン", "ガンビア", "カンボジア", "キタマケドニア", "ギニア", "ギニアビサウ", "キプロス", "キューバ", "ギリシャ", "キリバス", "キルギス", "グアテマラ", "クウェート", "クックショトウ", "グレナダ", "クロアチア", "ケニア", "コートジボワール", "コスタリカ", "コソボ", "コモロ", "コロンビア", "コンゴキョウワコク", "コンゴミンシュキョウワコク", "サウジアラビア", "サモア", "サントメプリンシペ", "ザンビア", "サンマリノ", "シエラレオネ", "ジブチ", "ジャマイカ", "ジョージア", "シリア", "シンガポール", "ジンバブエ", "スイス", "スウェーデン", "スーダン", "スペイン", "スリナム", "スリランカ", "スロバキア", "スロベニア", "セーシェル", "セキドウギニア", "セネガル", "セルビア", "セントクリストファーネービス", "セントビンセントグレナディーンショトウ", "セントルシア", "ソマリア", "ソロモンショトウ", "タイ", "ダイカンミンコク", "タジキスタン", "タンザニア", "チェコ", "チャド", "チュウオウアフリカ", "チュウカジンミンキョウワコク", "チュニジア", "チョウセンミンシュシュギジンミンキョウワコク", "チリ", "ツバル", "デンマーク", "ドイツ", "トーゴ", "ドミニカキョウワコク", "ドミニカコク", "トリニダードトバゴ", "トルクメニスタン", "トルコ", "トンガ", "ナイジェリア", "ナウル", "ナミビア", "ニウエ", "ニカラグア", "ニジェール", "ニホン", "ニュージーランド", "ネパール", "ノルウェー", "バーレーン", "ハイチ", "パキスタン", "バチカンシコク", "パナマ", "バヌアツ", "バハマ", "パプアニューギニア", "パラオ", "パラグアイ", "バルバドス", "ハンガリー", "バングラデシュ", "ヒガシティモール", "フィジー", "フィリピン", "フィンランド", "ブータン", "ブラジル", "フランス", "ブルガリア", "ブルキナファソ", "ブルネイ", "ブルンジ", "ベトナム", "ベナン", "ベネズエラ", "ベラルーシ", "ベリーズ", "ペルー", "ベルギー", "ポーランド", "ボスニアヘルツェゴビナ", "ボツワナ", "ボリビア", "ポルトガル", "ホンジュラス", "マーシャルショトウ", "マダガスカル", "マラウイ", "マリ", "マルタ", "マレーシア", "ミクロネシアレンポウ", "ミナミアフリカキョウワコク", "ミナミスーダン", "ミャンマー", "メキシコ", "モーリシャス", "モーリタニア", "モザンビーク", "モナコ", "モルディブ", "モルドバ", "モロッコ", "モンゴル", "モンテネグロ", "ヨルダン", "ラオス", "ラトビア", "リトアニア", "リビア", "リヒテンシュタイン", "リベリア", "ルーマニア", "ルクセンブルク", "ルワンダ", "レソト", "レバノン", "ロシア"],
+    "capital": ["アクラ", "アシガバット", "アスタナ", "アスマラ", "アスンシオン", "アディスアベバ", "アテネ", "アバルア", "アピア", "アブジャ", "アブダビ", "アムステルダム", "アルジェ", "アロフィ", "アンカラ", "アンタナナリボ", "アンドララベリャ", "アンマン", "イスラマバード", "ウィーン", "ウィントフック", "ウェリントン", "ウランバートル", "エルサレム", "エレバン", "オスロ", "オタワ", "カイロ", "カストリーズ", "カトマンズ", "カブール", "カラカス", "カンパラ", "キーウ", "キガリ", "キシナウ", "ギテガ", "キト", "キャンベラ", "キングスタウン", "キングストン", "キンシャサ", "グアテマラシティ", "クアラルンプール", "クウェート", "コナクリ", "コペンハーゲン", "ザグレブ", "サヌア", "サラエボ", "サンサルバドル", "サンティアゴ", "サントドミンゴ", "サントメ", "サンホセ", "サンマリノ", "ジブチ", "ジャカルタ", "ジュバ", "ジョージタウン", "シンガポール", "スコピエ", "ストックホルム", "スバ", "スリジャヤワルダナプラコッテ", "セントジョージズ", "セントジョンズ", "ソウル", "ソフィア", "ダカール", "タシケント", "ダッカ", "ダブリン", "ダマスカス", "タラワ", "タリン", "チュニス", "ティラナ", "ディリ", "ティンプー", "テグシガルパ", "テヘラン", "デリー", "トウキョウ", "ドゥシャンベ", "ドーハ", "ドドマ", "トビリシ", "トリポリ", "ナイロビ", "ナッソー", "ニアメ", "ニコシア", "ヌアクショット", "ヌクアロファ", "ネピドー", "バクー", "バグダッド", "バセテール", "パナマシティ", "バチカン", "ハノイ", "ハバナ", "ハボローネ", "バマコ", "パラマリボ", "ハラレ", "パリ", "パリキール", "ハルツーム", "バレッタ", "バンギ", "バンコク", "バンジュール", "バンダルスリブガワン", "ビエンチャン", "ビクトリア", "ビサウ", "ビシュケク", "ピョンヤン", "ビリニュス", "ファドゥーツ", "ブエノスアイレス", "ブカレスト", "ブダペスト", "フナフティ", "プノンペン", "プライア", "ブラザビル", "ブラジリア", "ブラチスラバ", "プラハ", "フリータウン", "プリシュティナ", "ブリッジタウン", "ブリュッセル", "プレトリア", "ベイルート", "ベオグラード", "ペキン", "ヘルシンキ", "ベルモパン", "ベルリン", "ベルン", "ポートオブスペイン", "ポートビラ", "ポートモレスビー", "ポートルイス", "ボゴタ", "ポドゴリツァ", "ホニアラ", "ポルトープランス", "ポルトノボ", "マジュロ", "マスカット", "マセル", "マドリード", "マナーマ", "マナグア", "マニラ", "マプト", "マラボ", "マルキョク", "マレ", "ミンスク", "ムババーネ", "メキシコシティ", "モガディシュ", "モスクワ", "モナコ", "モロニ", "モンテビデオ", "モンロビア", "ヤウンデ", "ヤムスクロ", "ヤレン", "ラパス", "ラバト", "リーブルビル", "リガ", "リスボン", "リマ", "リヤド", "リュブリャナ", "リロングウェ", "ルアンダ", "ルクセンブルク", "ルサカ", "レイキャビク", "ローマ", "ロゾー", "ロメ", "ロンドン", "ワガドゥグー", "ワシントンディーシー", "ワルシャワ", "ンジャメナ"]
+}
+
+def get_clean_char(w, pos="head", offset=0):
+    text = w.replace("ー", "")
+    if not text: return ""
+    char = text[offset] if pos == "head" else text[-(1+offset)]
     return SMALL_TO_LARGE.get(char, char)
 
+def shift_kana(char, n):
+    if char not in KANA_LIST: return char
+    return KANA_LIST[(KANA_LIST.index(char) + n) % len(KANA_LIST)]
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index(): return render_template('index.html')
+
+@app.route('/get_dictionary')
+def get_dictionary(): return jsonify(DICTIONARY_MASTER)
 
 @app.route('/search', methods=['POST'])
 def search():
     d = request.json
-    max_len = min(int(d.get('max_len', 5)), 8)
-    ignore_voiced = d.get('ignore_voiced', False)
-    must_chars = [c.translate(VOICE_MAP if ignore_voiced else {}) for c in d.get('must_char', "")]
-    limit_one = d.get('limit_one', False)
-    end_char = d.get('end_char', "").translate(VOICE_MAP if ignore_voiced else {})
+    max_len = int(d.get('max_len', 5))
+    pos_shift = int(d.get('pos_shift', 0))
+    use_shift = d.get('use_shift', False)
+    ks_val = int(d.get('ks_abs', 1))
+    shift_mode = d.get('shift_mode', 'abs')
+    use_multi_limit = d.get('use_multi_limit', False)
+    raw_mc = to_katakana(d.get('must_char', ""))
+    mc_counts = {}
+    must_chars = []
+    
+    if use_multi_limit:
+        for item in re.split('[、,]', raw_mc):
+            if ':' in item:
+                char, count = item.split(':')
+                mc_counts[to_katakana(char.strip())] = int(count)
+    else:
+        must_chars = [c for c in re.split('[、,]', raw_mc) if c]
+
+    exclude_conjugates = d.get('exclude_conjugates', False)
+    round_trip = d.get('round_trip', False)
+    once_constraint = d.get('once_constraint', False)
+    target_total_len = d.get('target_total_len')
+    
+    start_word = to_katakana(d.get('start_word', ""))
+    start_char = to_katakana(d.get('start_char', ""))
+    end_char = to_katakana(d.get('end_char', ""))
+    blocked_words = set(d.get('blocked_words', []))
+    force_words = set(d.get('force_words', []))
+    selected_cats = d.get('categories', ["country"])
+    
+    temp_pool = []
+    for cat in selected_cats:
+        for w in DICTIONARY_MASTER.get(cat, []):
+            if w not in blocked_words: temp_pool.append(w)
+    
+    if exclude_conjugates:
+        ht_list = [(get_clean_char(w, "head", 0), get_clean_char(w, "tail", 0)) for w in temp_pool]
+        counts = Counter(ht_list)
+        word_pool = [w for w in temp_pool if counts[(get_clean_char(w, "head", 0), get_clean_char(w, "tail", 0))] == 1]
+    else:
+        word_pool = list(set(temp_pool))
 
     results = []
+    start_time = time.time()
+    
+    def solve(path, current_total_len):
+        if time.time() - start_time > 15 or len(results) >= 1500: return
+        full_current = "".join(path)
+        
+        if use_multi_limit:
+            if any(full_current.count(c) > n for c, n in mc_counts.items()): return
 
-    def solve(path):
-        if len(results) >= 500: return
         if len(path) == max_len:
-            # 終了字チェック
-            if end_char:
-                tail = get_clean_char(path[-1], "tail").translate(VOICE_MAP if ignore_voiced else {})
-                if tail != end_char: return
+            if not force_words.issubset(set(path)): return
+            if use_multi_limit:
+                if not all(full_current.count(c) == n for c, n in mc_counts.items()): return
+            elif must_chars:
+                if once_constraint:
+                    if not all(full_current.count(mc) == 1 for mc in must_chars): return
+                else:
+                    if not all(mc in full_current for mc in must_chars): return
             
-            # 必須字チェック
-            if must_chars:
-                full = "".join(path).translate(VOICE_MAP if ignore_voiced else {})
-                if not all(mc in full for mc in must_chars): return
-                if limit_one and not all(full.count(mc) == 1 for mc in must_chars): return
-            
+            if target_total_len is not None and current_total_len != target_total_len: return
+            if end_char and get_clean_char(path[-1], "tail", 0) != end_char: return
             results.append(list(path))
             return
         
-        last_tail = get_clean_char(path[-1], "tail").translate(VOICE_MAP if ignore_voiced else {})
-        for next_w in COUNTRIES:
-            if next_w not in path:
-                next_head = get_clean_char(next_w, "head").translate(VOICE_MAP if ignore_voiced else {})
-                if next_head == last_tail:
-                    solve(path + [next_w])
+        last_word = path[-1]
+        is_even = len(path) % 2 == 0
+        src_char = get_clean_char(last_word, "head" if (round_trip and is_even) else "tail", pos_shift)
+        t_pos = "head" if not (round_trip and is_even) else "tail"
 
-    sw = d.get('start_word')
-    starts = [sw] if (sw in COUNTRIES) else COUNTRIES
-    for w in starts:
-        if not sw and d.get('start_char'):
-            if get_clean_char(w, "head").translate(VOICE_MAP if ignore_voiced else {}) != d.get('start_char').translate(VOICE_MAP if ignore_voiced else {}): continue
-        solve([w])
+        if use_shift:
+            targets = [shift_kana(src_char, abs(ks_val)), shift_kana(src_char, -abs(ks_val))] if shift_mode == 'abs' else [shift_kana(src_char, ks_val)]
+        else:
+            targets = [src_char]
 
+        for next_w in word_pool:
+            if next_w in path: continue
+            if get_clean_char(next_w, t_pos, pos_shift) in targets:
+                solve(path + [next_w], current_total_len + len(next_w))
+
+    starts = [start_word] if (start_word in word_pool) else word_pool
+    for w in sorted(starts):
+        if not start_word and start_char and get_clean_char(w, "head", pos_shift) != start_char: continue
+        solve([w], len(w))
+
+    results.sort(key=lambda x: (len("".join(x)), x))
     return jsonify({"routes": results, "count": len(results)})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, port=5000)
